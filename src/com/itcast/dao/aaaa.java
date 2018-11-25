f4bd6ac1f19ce8722da4ba09ee3e12f4337c6772
ligen <1396283974@qq.com>
2018-11-25 23:52:22 +0800

    qqqqqqqqqqqqqqq

---
+++ src/com/itcast/dao/aaaa.java
@@ -0,0 +1,12 @@
+package com.itcast.dao;
+
+public class aaaa {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaa");
+        System.out.println("aaaaaaaaa");
+        System.out.println("aaaaaaaaa");
+        System.out.println("aaaaaaaaa");
+        System.out.println("aaaaaaaaa");
+        System.out.println("aaaaaaaaa");
+    }
+}
